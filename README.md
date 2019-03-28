5e7992adb188f063624d3228e8839f10439507e4
Andrew Reddish <areddish@microsoft.com>
2019-03-28 10:17:19 -0700

    Initial implementation

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # custom-vision-download-project
-Example on how to download the data in a custom vision project
+Example how to download the data in a custom vision project.
+
+# How to use
+
+## Install Python
+
+*[Python](https://www.python.org/downloads/)
+*[Pip](https://pip.pypa.io/en/stable/installing/)
+
+## Get the Custom Vision SDK
+```
+pip install azure-cognitiveservices-vision-customvision
+```
+## Clone this repo
+```
+git clone https://github.com/areddish/custom-vision-download-project
+```
+## Find your project ID
+1. Navigate to https://customvision.ai
+2. Find the project you want to download
+3. Click on the Project
+4. Click on the gear icon (settings) and get the Project ID & your training key
+
+## Run the tool
+```
+python project_downloader -p "<project id from previous step>" -k "<your training key>"
+```
+
+This will emit some project info download the project images with some metadata into the current directory. To change the directory it downloads to pass the -d "path to where to download to" on the command line.
---
+++ project_downloader.py
@@ -0,0 +1,86 @@
+# Download a project
+
+from azure.cognitiveservices.vision.customvision.training import CustomVisionTrainingClient
+
+import os
+import sys
+import requests
+import argparse
+
+def download_image(uri, location):
+    req = requests.get(uri, stream=True)
+    if req.status_code == 200:
+        with open(location+i.id, 'wb') as image_file:
+            for chunk in req.iter_content(1024):
+                image_file.write(chunk)
+    else:
+        print("ERROR!: " + req.status_code)
+
+def download_images(trainer, project_id, location):
+    count = trainer.get_tagged_image_count(project_id)
+    print ("Found:",count,"tagged images.")
+
+    downloaded = 0
+    while(count > 0):
+        # Download the images in batches
+        count_to_download = min(count, 50)
+        print ("Getting", count_to_download, "images")
+        images = trainer.get_tagged_images(project_id, take=count_to_download, skip=downloaded)
+        for i in images:
+            print ("Downloading", i.id, i.original_image_uri)
+            download_image(i.original_image_uri, location)
+            print ("Creating meta data for: ", i.id)
+            with open(location+i.id+".metadata", 'wt') as image_file:
+                for t in i.tags:
+                    print("Tag:", t.tag_name, t.tag_id, , file = image_file)
+                for r in i.regions:
+                    print ("Region:", r.region_id, r.tag_id, r.left, r.top, r.width, r.height, file = image_file)
+                
+        downloaded += count_to_download
+        count -= count_to_download
+
+    # Downlaod any untagged images that may exist.
+    count = trainer.get_untagged_image_count(project_id)
+    print ("Found:", count, "untagged images.")
+    downloaded = 0
+    while(count > 0):
+        count_to_download = min(count, 50)
+        print ("Getting", count_to_download, "images")
+        images = trainer.get_untagged_images(project_id, take=count_to_download, skip=migrated)
+        for i in images:
+            print ("Downloading", i.id, i.original_image_uri)
+            download_image(i.original_image_uri, location)
+        downloaded += count_to_download
+        count -= count_to_download
+
+if __name__ == "__main__":
+    arg_parser = argparse.ArgumentParser()
+    arg_parser.add_argument("-p", "--project", action="store", type=str, help="Project ID", dest="project_id", default=None)
+    arg_parser.add_argument("-k", "--key", action="store", type=str, help="Training-Key", dest="training_key", default=None)
+    arg_parser.add_argument("-e", "--endpoint", action="store", type=str, help="Endoint", dest="endpoint", default="https://southcentralus.api.cognitive.microsoft.com")
+    arg_parser.add_argument("-d", "--dir", action="store", type=str, help="Target directory", dest="dir", default=".\\")
+    args = arg_parser.parse_args()
+
+    if (not args.project_id or not args.training_key):
+        arg_parser.print_help()
+        exit(-1)
+
+    print ("Collecting information for source project:", args.project_id)
+
+    # Create hte client
+    trainer = CustomVisionTrainingClient(args.training_key, endpoint=args.endpoint)
+
+    # Get the project
+    project = trainer.get_project(args.project_id)
+    print ("Downloading project:", project.name)
+    print ("\tDescription: ", project.description)
+    print ("\tDomain: ", project.settings.domain_id)
+
+    # get tags
+    tags = trainer.get_tags(project.id)
+    print ("Found:", len(tags), "tags.")
+    with open("tags.txt", "wt") as tags_file:
+        for tag in tags:
+            print(tag.name, tag.id, tag.type, file = tags_file)
+
+    download_images(trainer, project.id, args.dir)
